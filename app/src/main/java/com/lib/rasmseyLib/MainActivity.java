3dde194099da1325e2ab99de56459b94c399097d
曾明峰 <zengmf@seehoo.cn>
2019-01-23 15:48:47 +0800

    v1.3 最低版本 17

--- app/src/main/java/com/lib/rasmseyLib/MainActivity.java
+++ app/src/main/java/com/lib/rasmseyLib/MainActivity.java
@@ -3,11 +3,21 @@ package com.lib.rasmseyLib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rasmsey.library.CornerLabelButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    CornerLabelButton cornerLabelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cornerLabelButton = findViewById(R.id.ssssssss);
+        cornerLabelButton.setTv_title("莫斯科没有眼泪");
+        cornerLabelButton.setTv_numbeer("99");
+
+
     }
 }
