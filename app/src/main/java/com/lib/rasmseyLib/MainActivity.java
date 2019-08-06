d057c2f44dd158080e62bba162945c3a0a8c1c90
曾明峰 <zengmf@seehoo.cn>
2019-08-06 11:58:21 +0800

    1.文字自适应

--- app/src/main/java/com/lib/rasmseyLib/MainActivity.java
+++ app/src/main/java/com/lib/rasmseyLib/MainActivity.java
@@ -3,21 +3,11 @@ package com.lib.rasmseyLib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.rasmsey.library.CornerLabelButton;
-
 public class MainActivity extends AppCompatActivity {
-
-    CornerLabelButton cornerLabelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        cornerLabelButton = findViewById(R.id.ssssssss);
-        cornerLabelButton.setTv_title("莫斯科没有眼泪");
-        cornerLabelButton.setTv_numbeer("99");
-
-
     }
 }
